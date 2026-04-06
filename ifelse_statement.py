ca6adf4a47759cc0bbee535c4c81c946ac76edfd
bonnyndush99 <bonnyndush99@gmail.com>
2026-04-05 23:56:27 -0400

    Update ifelse_statement.py
    Comparing Strings

--- ifelse_statement.py
+++ ifelse_statement.py
@@ -2,3 +2,9 @@ if year % 4 == 0:
     days = 366
 else:
     days = 365
+
+#Assume the variable s has been assigned a string. Write code that displays "stop" if s is equal to "end". Otherwise, display "go".#
+    if s == "end":
+    print("stop")
+else:
+    print("go")
